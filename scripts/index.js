d90a0a3ec542ff80dca2f537f33efcd42bfc85d7
Nathaniel Daley <ndaley64@gmail.com>
2024-09-07 16:43:56 -0500

    Fix canvas stretching

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,15 @@ canvasContext.rect(20, 40, 50, 50);
 canvasContext.fillStyle = "#FF0000";
 canvasContext.fill();
 canvasContext.closePath();
+
+canvasContext.beginPath();
+canvasContext.arc(240, 160, 20, 0, Math.PI * 2, false);
+canvasContext.fillStyle = "green";
+canvasContext.fill();
+canvasContext.closePath();
+
+canvasContext.beginPath();
+canvasContext.rect(160, 10, 100, 40);
+canvasContext.strokeStyle = "rgb(0 0 255 / 50%)";
+canvasContext.stroke();
+canvasContext.closePath();
